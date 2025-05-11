bda202b0fc438df9e2866c898f740a0787238574
Teja445 <marajutej@gmail.com>
2025-05-11 12:52:25 +0530

    Raviteja | add region, cidr block range variables
    add region, cidr block range variables

--- without_module/variable.tf
+++ without_module/variable.tf
@@ -1,1 +1,14 @@
 ## add variables here
+
+variable "region" {
+type = string
+default = "us-east-1"
+description = "AWS region"
+}
+
+variable "cidr_block" {
+type = string
+default = "10.10.0.0/16"
+
+}
+
